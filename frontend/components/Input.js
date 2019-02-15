12e69427a405e37fd10a372cdcf268e5520b615d
Aaroh Mankad <aarohmankad@gmail.com>
2019-02-14 18:09:38 -0800

    add preliminary style

--- frontend/components/Input.js
+++ frontend/components/Input.js
@@ -2,12 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import { transparentize } from 'polished';
 
-// background: #ffa9f0;
 const StyledInput = styled.input`
   background: ${({ theme }) => transparentize(0.75, theme.white)};
   border: none;
   color: ${({ theme }) => theme.white};
-  font-size: 18px;
+  font-size: 20px;
   padding: 5px;
   margin: 5px;
 
