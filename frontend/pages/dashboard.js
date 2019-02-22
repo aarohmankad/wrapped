b9abfa20ff811b8a6a8e8d3bbe2ed08f9f96ca69
Aaroh Mankad <aarohmankad@gmail.com>
2019-02-22 11:27:12 -0800

    add modal for creation dialog

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -1,6 +1,47 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { Component } from 'react';
+import router from 'next/router';
 
-import ActionButton from '../components/Button/ActionButton';
+import CreateEvent from '../components/CreateEvent';
+import Link from '../components/Link';
+import Navigation from '../components/Navigation';
+import ROUTES from '../constants/ROUTES';
+import { withFirebase } from '../components/Firebase';
 
-export default () => <ActionButton>Dashboard</ActionButton>;
+const INITIAL_STATE = {};
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onChange = event =>
+    this.setState({ [event.target.name]: event.target.value });
+
+  onSubmit = event => {
+    const { email, password } = this.state;
+    this.props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(authUser => {
+        this.setState({ ...INITIAL_STATE });
+        router.push(ROUTES.HOME.path);
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <h1>Dashboard</h1>
+        <CreateEvent />
+      </div>
+    );
+  }
+}
+
+export default withFirebase(Dashboard);
