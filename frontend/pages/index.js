9c2562397857ba29d315123a307e295098c9335d
alex <ate001@ucr.edu>
2019-02-25 22:16:33 -0800

    Want to add our pictures to home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,6 +7,7 @@ import Navigation from '../components/Navigation';
 //its and img. then we can center and make it look nice
 //ex was like for a header. can use h1 h2 etc..
 //but can put it in a div and style it to be the size, position etc.
+
 const Logo = styled.img`
   width: 75px;
   height 75px;
@@ -16,8 +17,9 @@ const Logo = styled.img`
   left: 25px;
 `;
 
-const Title2 = styled.div`
-	color: blue;
+const Description = styled.div`
+	color: ${({ theme }) => theme.black};
+	font-size: 23px;
 `;
 
 const Container = styled.div`
@@ -28,7 +30,8 @@ const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
-	font-size: 30px;
+	// font-size: 30px;
+	top: -2px;
 	align-items: center;
 	& input {
 		width: 100%;
@@ -44,6 +47,7 @@ const Title = styled.div`
 	font-size: 72px;
 	ajustify-content: start;
 	top: 750px;
+	text-align: right;
 `;
 
 const Home = props => {
@@ -52,11 +56,12 @@ const Home = props => {
 			<Navigation />
 			<Logo src="../static/wrapped_logo.png" alt="wrapped_logo" />
 			<Container>
-				<Title>wrapped.</Title>
-				<Title2>
+				{/*<Title>wrapped.</Title>*/}
+				<Description>
 					At wrapped, our goal is to make it easy for you to show
-					appreciation for your loved ones with the perfect gift.
-				</Title2>
+					appreciation for your loved ones with the perfect gift,
+					every time.
+				</Description>
 			</Container>
 		</div>
 	);
